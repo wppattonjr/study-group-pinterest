2573809d19608f065066f3435ebbe7b908c3b13f
Wendell <wendell.pattonjr@gmail.com>
2020-11-06 22:14:40 -0600

    made the spacing of the cards look a little better

--- src/javascripts/components/navBar/myNavbar.js
+++ src/javascripts/components/navBar/myNavbar.js
@@ -14,7 +14,6 @@ const logoutEvent = () => {
 const myNavbar = (currentUser) => {
   $('#nav').html(
     `<nav class="navbar navbar-expand-lg navbar-light bg-light">
-    <a class="navbar-brand" href="#">Sort of Pinterest</a>
     <button class="navbar-toggler" type="button" data-toggle="collapse" 
     data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
       <span class="navbar-toggler-icon"></span>
@@ -22,16 +21,19 @@ const myNavbar = (currentUser) => {
 
     <div class="collapse navbar-collapse" id="navbarSupportedContent">
     <ul class="navbar-nav mr-auto">
-    <li class="nav-item mx-3"  id="boards-link">
-      <a id="add-board-link" class="nav-link" href="#"><i class="fas fa-lightbulb-off"></i> Boards</a>
-    </li>
-    <li class="nav-item mx-3" id="add-board-link">
-      <a class-"nav-link" href="#"><i class="fas fa-plus-circle"></i> Add A Board</a>
-    </li>
-    <li class="nav-item mx-3"  id="add-pin-link">
-      <a class="nav-link" href="#"><i class="fas fa-lightbulb-off"></i> Add A Pin</a>
-    </li>
-  </ul>
+      <li class="nav-item mx-3"  id="boards-link">
+        <a class="navbar-brand" href="#">Sort of Pinterest</a>
+      </li>
+      <li>
+        <a id="boards-link" class="nav-link" href="#"><i class="fas fa-lightbulb-off"></i> Boards</a>
+      </li>
+      <li class="nav-item mx-3" id="add-board-link">
+        <a class-"nav-link" href="#"><i class="fas fa-plus-circle"></i> Add A Board</a>
+      </li>
+      <li class="nav-item mx-3"  id="add-pin-link">
+        <a class="nav-link" href="#"><i class="fas fa-lightbulb-off"></i> Add A Pin</a>
+      </li>
+    </ul>
 
       <ul class="navbar-nav ml-auto">
         <li class="user-info-nav">
